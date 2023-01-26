9ed2edb7950293fd599fe7b82ece336f8143ca41
erikbayerlein <erik.bayer@icloud.com>
2023-01-26 15:47:40 -0300

    Project to learn more about API's

---
+++ Weather-Project/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=29cb817bc95cdb3c95c3da61c99d7eec";
+
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            console.log(temp);
+            console.log(weatherDescription);
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius</h1>");
+            res.write("<p>The weather is " + weatherDescription + "</p>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        });
+    });
+});
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000");
+});
